48e8ed365bae50a19f80cf88bd35843234f53c33
Nuriman <nurimanuyghur@Nurimans-MacBook-Pro.local>
2019-07-31 16:01:13 -0400

    Initial commitvsduvusvduausDFUfwd

---
+++ src/test/java/com/cybertek/test1.java
@@ -0,0 +1,15 @@
+package com.cybertek;
+
+public class test1 {
+
+
+    public static void main(String[] args) {
+
+
+
+        String abc = "dbiasusauivduasvckyvosuDVOY";
+
+        System.out.println(abc);
+
+    }
+}
